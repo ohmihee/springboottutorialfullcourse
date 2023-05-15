1c2db7258bca86c9748d5e6f5001f6ca4a0dabe6
mihee <algml9603@gmail.com>
2023-05-15 23:12:05 +0900

    feat: [swagger]: init  - 0515

--- src/main/java/com/example/javafullcourse/JavaFullCourse.java
+++ src/main/java/com/example/javafullcourse/JavaFullCourse.java
@@ -2,6 +2,7 @@ package com.example.javafullcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 //@SpringBootApplication(scanBasePackages = {"com.example.springBootJoinAndLogin"})
 @SpringBootApplication
@@ -9,6 +10,12 @@ public class JavaFullCourse {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaFullCourse.class, args);
+    }
+
+    @GetMapping
+    public void test() {
+        System.out.println("test===");
+
     }
 
 }
